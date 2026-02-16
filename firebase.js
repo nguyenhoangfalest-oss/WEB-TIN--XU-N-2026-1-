f4c4c4907096d55f4ee092530c76571c1c251327
nguyenhoangfalest-oss <nguyenhoangfalest@gmail.com>
2026-02-16 20:22:43 +0700

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDfZPIg6Nif_Mx_Wwyl0byM6vJCd5BLgo8",
+  authDomain: "xuanbinhngo-2026.firebaseapp.com",
+  projectId: "xuanbinhngo-2026",
+  storageBucket: "xuanbinhngo-2026.appspot.com",
+  messagingSenderId: "910448630867",
+  appId: "1:910448630867:web:2b48e0b859e355aa0efaa6",
+  measurementId: "G-FN0BFL9FQQ"
+};
+
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
